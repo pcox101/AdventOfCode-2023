ad807053b72a31e2065b5476432356c85d12f956
Peter Cox <Peter.Cox@kingfisher.com>
2023-12-22 14:47:25 +0000

    Day 21 (Part 2)

--- Day21/src/main/java/org/example/Main.java
+++ Day21/src/main/java/org/example/Main.java
@@ -122,26 +122,13 @@ public class Main {
         Set<Position> positions = new HashSet<>();
         positions.add(new Position(startingRow, startingColumn));
 
-        Set<String> loopDetection = new HashSet<>();
-        boolean discoveredLoop = false;
-        long startLoop = 0;
-        long loopLength = 0;
-        Set<String> loopDetection2 = new HashSet<>();
-        boolean discoveredLoop2 = false;
-        long startLoop2 = 0;
-        long loopLength2 = 0;
-        Set<String> loopDetection3 = new HashSet<>();
-        boolean discoveredLoop3 = false;
-        long startLoop3 = 0;
-        long loopLength3 = 0;
-
         int width = gameBoard[0].length;
         int height = gameBoard.length;
+
+        int[] fofn = new int[3];
+
         for (int counter = 0; counter < 500; counter++)
         {
-            if (counter % 100000 == 0)
-                System.out.println(counter);
-
             Set<Position> newPositions = new HashSet<>();
             for (Position position: positions)
             {
@@ -170,65 +157,60 @@ public class Main {
             positions = newPositions;
             //outputGameBoard(gameBoard, positions);
 
-            if (!discoveredLoop) {
-                String hash = generateHashForMap(gameBoard, positions, 0);
-                // Check the main map for loops
-                if (loopDetection.contains(hash)) {
-                    if (startLoop == 0) {
-                        startLoop = counter;
-                        loopDetection.clear();
-                        loopDetection.add(hash);
-                    } else {
-                        loopLength = counter - startLoop;
-                        discoveredLoop = true;
-                        System.out.printf("Found loop at %d of length %d (%s)%n", startLoop, loopLength, hash);
-                    }
-                } else {
-                    loopDetection.add(hash);
-                }
+            // Pick some numbers
+            // We need 26501365 steps. Which is 65 + (131 * 202300)
+            // 65 + 0 * 131 = 65
+            // 65 + 1 * 131 = 196
+            // 65 + 2 * 131 = 327
+            if ((counter + 1) == 65) // n = 0
+            {
+                System.out.printf("Value at %d (n=%d) is %d%n", counter + 1, 0, positions.size());
+                fofn[0] = positions.size();
             }
-            else if (!discoveredLoop2) {
-                String hash = generateHashForMap(gameBoard, positions, gameBoard.length);
-                if (loopDetection2.contains(hash)) {
-                    if (startLoop2 == 0) {
-                        startLoop2 = counter;
-                        loopDetection2.clear();
-                        loopDetection2.add(hash);
-                    }
-                    else
-                    {
-                        loopLength2 = counter - startLoop2;
-                        discoveredLoop2 = true;
-                        System.out.printf("Found loop 2 at %d of length %d (%s)%n",startLoop2, loopLength2, hash);
-                    }
-                }
-                else {
-                    loopDetection2.add(hash);
-                }
+            if ((counter + 1) == 196) // n = 1
+            {
+                System.out.printf("Value at %d (n=%d) is %d%n", counter + 1, 1, positions.size());
+                fofn[1] = positions.size();
             }
-            else if (!discoveredLoop3) {
-                String hash = generateHashForMap(gameBoard, positions, gameBoard.length * 2);
-                if (loopDetection3.contains(hash)) {
-                    if (startLoop3 == 0) {
-                        startLoop3 = counter;
-                        loopDetection3.clear();
-                        loopDetection3.add(hash);
-                    }
-                    else
-                    {
-                        loopLength3 = counter - startLoop3;
-                        discoveredLoop3 = true;
-                        System.out.printf("Found loop 3 at %d of length %d (%s)%n",startLoop3, loopLength3, hash);
-                    }
-                }
-                else {
-                    loopDetection3.add(hash);
-                }
+            if ((counter + 1) == 327) // n = 2
+            {
+                System.out.printf("Value at %d (n=%d) is %d%n", counter + 1, 2, positions.size());
+                fofn[2] = positions.size();
             }
+            if ((counter + 1) == 458) // n = 3 (for verification only)
+                System.out.printf("Value at %d (n=%d) is %d%n", counter + 1, 3, positions.size());
+
         }
-        return positions.size();
+
+        // Use lagrange (2nd order) to calculate the polynomial
+        // https://mathworld.wolfram.com/LagrangeInterpolatingPolynomial.html
+        // We want to solve for x = 202300
+        long sum = 0;
+        long x = 202300;
+        // n = 3...
+        for (int j = 0; j < 3; j++)
+        {
+            sum += getInner(fofn, x, j);
+        }
+
+        return sum;
     }
 
+    private static long getInner(int[] fofn, long x, int j)
+    {
+        long dividend = 1;
+        long divisor = 1;
+
+        for (long k = 0; k < 3; k++)
+        {
+            if (k == (long)j)
+                continue;
+            dividend *= (x - k);
+            divisor *= ((long)j - k);
+        }
+
+        return fofn[j] * (dividend / divisor);
+    }
 
     private static char[][] getCharArrayGameBoard(List<String> gameBoard)
     {
